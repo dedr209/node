e3b028afacbe9c3f847097731a37b441ba9fb523
dedr <060806andrew>
2026-05-09 16:13:30 +0300

    refactor: update validation schema type to ZodType<unknown>

--- src/middleware/validate.ts
+++ src/middleware/validate.ts
@@ -1,7 +1,7 @@
 import type { Request, RequestHandler, Response } from "express";
-import type { ZodTypeAny } from "zod";
+import type { ZodType } from "zod";
 
-export const validate = (schema: ZodTypeAny): RequestHandler => {
+export const validate = (schema: ZodType<unknown>): RequestHandler => {
   return (req: Request, _res: Response, next) => {
     try {
       req.body = schema.parse(req.body);
