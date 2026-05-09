41666a629ea8eda428162e81c6b0cc03af732278
dedr <060806andrew>
2026-05-09 15:43:23 +0300

    feat: add filtering functionality to MovieStorage for genre and director

--- src/storage/movie.storage.ts
+++ src/storage/movie.storage.ts
@@ -7,6 +7,7 @@ import type {
 } from "../schemas/movie.schema";
 
 type Movie = MovieEntity;
+type MovieFilters = { genre?: string; director?: string };
 
 export class MovieStorage {
   private movies = new Map<string, Movie>();
@@ -26,6 +27,25 @@ export class MovieStorage {
 
   getById(id: string): Movie | undefined {
     return this.movies.get(id);
+  }
+
+  getAll(filters: MovieFilters = {}): Movie[] {
+    const { genre, director } = filters;
+
+    return [...this.movies.values()].filter((movie) => {
+      if (genre && movie.genre !== genre) {
+        return false;
+      }
+
+      if (director) {
+        const movieDirector = (movie as Movie & { director?: string }).director;
+        if (movieDirector !== director) {
+          return false;
+        }
+      }
+
+      return true;
+    });
   }
 
   update(id: string, data: UpdateMovieInput): Movie | undefined {
